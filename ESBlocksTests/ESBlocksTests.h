9f43bb72995c8f92b1ee1bb5fffbc9950ab46f45
Chi Zhang <elecpaoao@gmail.com>
2012-06-21 14:36:42 +0800

    Initial Commit

---
+++ ESBlocksTests/ESBlocksTests.h
@@ -0,0 +1,13 @@
+//
+//  ESBlocksTests.h
+//  ESBlocksTests
+//
+//  Created by Chi Zhang on 6/21/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface ESBlocksTests : SenTestCase
+
+@end
---
+++ ESBlocksTests/ESBlocksTests.m
@@ -0,0 +1,32 @@
+//
+//  ESBlocksTests.m
+//  ESBlocksTests
+//
+//  Created by Chi Zhang on 6/21/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "ESBlocksTests.h"
+
+@implementation ESBlocksTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in ESBlocksTests");
+}
+
+@end
